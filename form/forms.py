e30008208ff1230f4a43a4fcbdf88f602d97a603
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2020-01-15 17:59:21 +0330

    adding bootstrap to project

--- form/forms.py
+++ form/forms.py
@@ -2,5 +2,13 @@
 from django import forms
 
 class MyForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=50)
-    last_name  = forms.CharField(label='Last Name', max_length=50)
+    first_name = forms.CharField(
+        label='First Name',
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': "form-control"})\
+    )                                
+    last_name = forms.CharField(
+        label='Last Name',
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': "form-control"})
+    )
