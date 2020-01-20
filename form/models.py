e6c792bbf1553877e771d5fe6f6761a4b8a01ca2
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2020-01-20 09:52:34 +0330

    add model

--- form/models.py
+++ form/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import datetime
 
-# class PersonalInfo(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     genderType = ( ('M','Male') , ('F','Female') )
-#     gender = models.CharField(choices=genderType, max_length=1)    
-#     date_of_birth = models.DateField()
+class PersonalInfo(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    genderType = ( ('M','Male') , ('F','Female') )
+    gender = models.CharField(choices=genderType,default='M', max_length=1)    
+    date_of_birth = models.DateField(datetime.date.today)
