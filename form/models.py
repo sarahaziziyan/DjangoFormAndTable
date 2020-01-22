6ed084d1bb83697d28f6121b03044ba637816177
Sarah Aziziyan <aziziyan.sarah@gmail.com>
2020-01-22 17:05:09 +0330

    add nationalCode as the primary_key

--- form/models.py
+++ form/models.py
@@ -2,8 +2,9 @@ from django.db import models
 import datetime
 
 class PersonalInfo(models.Model):
+    nationalCode = models.CharField(max_length=10,primary_key=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     genderType = ( ('M','Male') , ('F','Female') )
     gender = models.CharField(choices=genderType,default='M', max_length=1)    
-    date_of_birth = models.DateField(datetime.date.today)
+    date_of_birth = models.DateField()
